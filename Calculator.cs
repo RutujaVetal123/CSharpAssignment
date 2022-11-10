286d7f7149b887422ccb9d7ddbc706ccada00d81
RutujaVetal123 <rutujavetal@gmail.com>
2022-11-10 05:35:50 +0530

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    class Calculator
+    {
+        #region Calculator
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public double Division(int a, int b)
+        {
+            return a / b;
+        }
+        #endregion
+
+        #region Highest marks
+        public int MaxMarks(int[] marks)
+        {
+            int max = marks[0];
+            for (int i = 1; i < 5; i++)
+                if (marks[i] > max)
+                    max = marks[i];
+            return max;
+        }
+        #endregion
+
+        #region Sum of n numbers
+        public int SumOfNum(int[] array)
+        {
+            int sum = 0;
+            for (int i=0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        #endregion
+
+        #region Swap
+        public void Swap(int num4, int num5)
+        {
+            int temp = num4;
+            num4 = num5;
+            num5 = temp;
+            Console.WriteLine("Swaped Numbers : "+num4+" "+num5);
+        }
+        #endregion
+
+        #region Area and Circumference of Circle
+        public void AreaCircumOfCircle(double radius)
+        {
+            double PI = Math.PI;
+            double Area = PI *radius*radius;
+
+            double Circumference = 2 * PI * radius;
+            Console.WriteLine("Circle of radius "+radius+ " has Area : " + Area +" and Circumference : "+ Circumference);
+
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Calculator
+            // Task 1 ************************************************************//
+            /* Calculator
+             * Addition
+             * Subtraction
+             * Multiplication
+             * Division*/
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("\t Calculator\n Enter Two Numbers : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t Operations can be performend "+
+                "\n 1. Addtion\n 2. Subtraction\n 3. Multiplication\n 4. Division"+
+                "\n Answer : ");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            double div = 0;
+
+            switch (ans)
+            {
+                case 1:
+                    result = cal.Addition(num1, num2);
+                    Console.WriteLine("Addition = " + result);
+                    break;
+
+                case 2:
+                    result = cal.Subtraction(num1, num2);
+                    Console.WriteLine("Subtraction = " + result);
+                    break;
+
+                case 3:
+                    result = cal.Multiplication(num1, num2);
+                    Console.WriteLine("Multiplication = " + result);
+                    break;
+
+                case 4:
+                    div = cal.Division(num1, num2);
+                    Console.WriteLine("Division = " + div);
+                    break;
+
+                default:
+                    Console.WriteLine("INVALID OPERATION");
+                    break;
+            }
+            #endregion
+
+
+
+            Console.WriteLine("\n\n************************************************************");
+            // Task 2 ************************************************************//
+            /*  
+             *  Accept average marks of five students. Display the highest marks obtained 
+             */
+
+            int[] marks = new int[5];
+            Console.WriteLine("\t Highest Marks\n Enter five Students score :");
+            for (int i = 0; i < 5; i++)
+            {
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maxMarks = cal.MaxMarks(marks);
+            Console.WriteLine("Highest Score = " + maxMarks);
+
+
+
+            Console.WriteLine("\n\n************************************************************");
+            // Task 3 ************************************************************//
+            /*  
+             * The method should find the sum of all the integers passed and display the result. 
+             */
+
+            Console.WriteLine("\t Sum of \'n\' integer\n How many numbers do you want to add? :");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num3 > 0) 
+            {
+                int[] array = new int[num3];
+                Console.WriteLine("Enter the " + num3 + " integers : ");
+                for (int i = 0; i < num3; i++)
+                {
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int sum = cal.SumOfNum(array);
+                Console.WriteLine("Sum of "+num3+" integers = " + sum);
+            }
+            else
+            {
+                Console.WriteLine("INVALID NUMBER (SHOULD BE NON-ZERO AND POSITIVE)");
+            }
+
+
+            Console.WriteLine("\n\n************************************************************");
+            // Task 4 ************************************************************//
+            /*  
+             *  Method to swap two integer and print the swapped value.
+             */
+
+            Console.WriteLine("\t Swappin of numbers\n Enter Two Numbers : ");
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
+            cal.Swap(num4, num5);
+
+
+            Console.WriteLine("\n\n************************************************************");
+            // Task 5 ************************************************************//
+            /*  
+             * Method that calculates the area and circumference of the circle. 
+             */
+
+            Console.WriteLine("\t Circumference and Area of Circle\n Enter the radius : ");
+            double radius = Convert.ToInt32(Console.ReadLine());
+            cal.AreaCircumOfCircle(radius);
+
+
+            Console.WriteLine("\n\n************************************************************");
+            // Task 6 ************************************************************//
+            /*  
+             * Create a structure Book
+             * a. Accept the details of the book
+             * b. Display the details of the book. The type of book should be displayed as a string e.g.: Magazine
+             */
+
+        }
+    }
+}
